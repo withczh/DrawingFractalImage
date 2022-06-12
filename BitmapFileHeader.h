711d90722d4f7d62e3b5424c7045ef565210e067
withczh <withczh@qq.com>
2022-06-12 21:54:43 +0800

    bmpFileHeader

--- BitmapFileHeader.h
+++ BitmapFileHeader.h
@@ -4,12 +4,13 @@
 #include<cstdint>
 
 /*位图文件头*/
+#pragma pack(2)  //2字对齐
 struct BitmapFileHeader
 {
-    char header[2]={'B','M'};
-    int32_t fileSize;
-    int32_t reserve={0};
-    int32_t dataOffest;
+    char header[2]={'B','M'}; //位图文件类型
+    int32_t fileSize;   //位图文件大小
+    int32_t reserve={0};    //文图文件保留字
+    int32_t dataOffest; //位图数据的偏移量
 };
 
 #endif
