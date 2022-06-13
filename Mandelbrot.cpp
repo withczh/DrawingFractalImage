4a1cba69673db49248cf66adaa40e173374e8b7d
withczh <withczh@qq.com>
2022-06-13 16:24:24 +0800

    Mandelbrot algorithm

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -1,4 +1,5 @@
 #include "Mandelbrot.h"
+#include<complex>
 
 namespace czh
 {
@@ -9,6 +10,19 @@ Mandelbrot::~Mandelbrot(){}
 
 int Mandelbrot::getIterations(double x,double y)
 {
+    std::complex<double> z=0;
+    std::complex<double> c(x,y);
+
+    int Iteration=0;
+    while(Iteration<MAX_ITERATION)
+    {
+        z=z*z+c;
+        if(abs(z)>2)
+        {
+            break;
+        }
+        Iteration++;
+    }
     return 0;
 }
 
