c42d9b68e463a507808c1261e78aa211615bfc7d
withczh <withczh@qq.com>
2022-06-14 09:32:42 +0800

    reorganising main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
 #include<iostream>
-#include "Bitmap.h"
+
+#include<math.h>
+
+#include "FractalCreator.h"
 using namespace czh;
 
 int main()
 {
-    const int width = 800;
-    const int height = 600;
-    Bitmap bitmap(width,height);
-    
-    for(int y=0;y<height;y++)
-    {
-        for(int x=0;x<width;x++)
-        {
-            bitmap.setPixel(x,y,255,0,0);
-        }
-    }
-    
-    
-    bitmap.setPixel(width/2,height/2,255,255,255);
-    bitmap.write("test.bmp");
+    const int height=600;
+    FractalCreator fractalCreator(800,600);
+    fractalCreator.addZoom(Zoom(285,height-202,0.001));
+    fractalCreator.run("Fractal.bmp");
 
     std::cout<<"finished"<<std::endl;
 
